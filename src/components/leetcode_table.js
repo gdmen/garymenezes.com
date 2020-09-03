dbce756d42620f41c914235b67a334f1df6813ca
gdmen <gmenezes@alumni.upenn.edu>
2020-09-03 11:31:53 -0700

    Add a basic table of problems to LeetCode book.

---
+++ src/components/leetcode_table.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
+
+import styles from "./leetcode_table.module.css"
+
+const difficultyText = { 0: "easy", 1: "medium", 2: "hard" }
+
+const LeetCodeTable = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(
+        sort: { fields: [frontmatter___number], order: ASC }
+        filter: { fields: { type: { eq: "note" }, book: { eq: "leetcode" } } }
+      ) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              difficulty
+              number
+              title
+              url
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <table className={styles.table}>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Difficulty</th>
+      </tr>
+      {data.allMdx.edges.map(({ node }) => (
+        <tr key={node.id}>
+          <td>{node.frontmatter.number}</td>
+          <td>
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+          </td>
+          <td>
+            <span
+              className={
+                node.frontmatter.difficulty === 0
+                  ? styles.easy
+                  : node.frontmatter.difficulty === 1
+                  ? styles.medium
+                  : styles.hard
+              }
+            >
+              {difficultyText[node.frontmatter.difficulty]}
+            </span>
+          </td>
+        </tr>
+      ))}
+    </table>
+  )
+}
+
+export default LeetCodeTable
