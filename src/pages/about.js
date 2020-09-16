574d954e0a375e110f1497f565f28ea4bb60a764
gdmen <gmenezes@alumni.upenn.edu>
2020-09-16 11:00:00 -0700

    Add bio to 'about' page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
 import EmploymentBlock from "../components/employment_block"
 
 import styles from "./about.module.css"
 
 export default function About({ data }) {
+  const about = data.about.edges[0].node
   return (
     <Layout page="about">
       <section>
+        <h2 className={styles.about_title}>ABOUT ME</h2>
+        <div className={`mdx ${styles.about_content}`}>
+          <MDXRenderer>{about.body}</MDXRenderer>
+        </div>
+      </section>
+      <section>
         <ul className={styles.employment_list}>
-          {data.allMdx.edges.map(({ node }) => (
+          {data.employment.edges.map(({ node }) => (
             <li key={node.id}>
               <EmploymentBlock
                 company={node.frontmatter.company}
@@ -31,7 +39,18 @@ export default function About({ data }) {
 
 export const query = graphql`
   query {
-    allMdx(
+    about: allMdx(filter: { fields: { type: { eq: "about" } } }) {
+      edges {
+        node {
+          id
+          body
+          frontmatter {
+            date
+          }
+        }
+      }
+    }
+    employment: allMdx(
       sort: { fields: [frontmatter___start], order: DESC }
       filter: { fields: { type: { eq: "employment" } } }
     ) {
