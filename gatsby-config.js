1fb2a9cc253fe11c096b5f4e9c5ed919157935c2
gdmen <gmenezes@alumni.upenn.edu>
2020-09-24 10:55:09 -0700

    Update gatsby-plugin-mdx config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`],
         defaultLayouts: {
           default: require.resolve("./src/components/layout.js"),
         },
