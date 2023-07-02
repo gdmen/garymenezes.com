5c232f641c54818112253cc2b3237bb9b1134e6f
gdmen <gmenezes@alumni.upenn.edu>
2023-07-02 15:12:34 -0700

    Add all weights for downloaded font.

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Karla`],
+        fonts: [
+          `Karla\:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i`
+        ],
       },
     },
     {
